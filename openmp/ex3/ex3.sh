f9fb4bcf99d3b5fe0eda122d3f54b30eadbcdc32
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-02-24 12:06:13 +0000

    Add slurm submission scripts

---
+++ openmp/ex3/ex3.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH -p fct
+#SBATCH --ntasks-per-node=4
+#SBATCH --nodes=1
+#SBATCH --qos=uvlabuaveiro
+
+#
+# Build using:
+# module load gcc-14.1
+# cmake --build .
+#
+# or:
+# module load gcc-14.1
+# cmake .
+# make
+#
+
+module load gcc-14.1
+./ex3
