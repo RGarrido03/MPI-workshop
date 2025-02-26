f912ac0377a6b44b46337e01671d39a2ddee42fb
Rúben Garrido <57329376+RGarrido03@users.noreply.github.com>
2025-02-26 12:18:47 +0000

    Change number of nodes (only 1 server avail?)

--- mpi/ex1/ex1.sh
+++ mpi/ex1/ex1.sh
@@ -2,7 +2,7 @@
 
 #SBATCH -p hpc
 #SBATCH --ntasks-per-node=4
-#SBATCH --nodes=4
+#SBATCH --nodes=1
 #SBATCH --qos=normal
 
 export MODULEPATH="/etc/scl/modulefiles:/cvmfs/sw.el8/modules/hpc/main:/cvmfs/sw.el8/modules/hpc/aoc40:/cvmfs/sw.el8/modules/hpc/gcc85:/cvmfs/sw.el8/modules/hpc/gcc11:/cvmfs/sw.el8/modules/hpc/gcc13:/cvmfs/sw.el8/modules/hpc/intel:/cvmfs/sw.el8/modules/gpu:/cvmfs/sw.el8/modules/ml:/cvmfs/sw.el8/modules/bio"
